2c447379c0ea3c76ea4de56efccf71c57e0260ce
NatalieBlack <natalievictoriablack@gmail.com>
2017-04-03 11:56:25 -0400

    in-class code for second half of 11-debugging-testing

--- 11-debugging-and-testing-in-ruby/temperature.rb
+++ 11-debugging-and-testing-in-ruby/temperature.rb
@@ -1,3 +1,10 @@
 # This is where the method we're testing will go
 #  (T(°F) - 32) × 5/9
 #  T(°C) × 9/5 + 32
+def f_to_c(f)
+  return (f - 32) * 5.0/9.0
+end
+
+def c_to_f(c)
+  return c * 9.0/5.0 + 32
+end
--- 11-debugging-and-testing-in-ruby/test_temperature.rb
+++ 11-debugging-and-testing-in-ruby/test_temperature.rb
@@ -3,5 +3,45 @@ require 'minitest/pride'
 require_relative 'temperature.rb'
 
 class TestTemperature < MiniTest::Test
-   # tests go in here
+   def test_f_to_c
+     expected_value = 10
+     actual_value = f_to_c(50)
+
+     assert_equal(expected_value, actual_value)
+   end
+
+   def test_f_to_c_negative
+     expected_value = -20
+     actual_value = f_to_c(-4)
+
+     assert_equal(expected_value, actual_value)
+   end
+
+   def test_f_to_c_freezing
+     expected_value = 0
+     actual_value = f_to_c(32)
+
+     assert_equal(expected_value, actual_value)
+   end
+
+   def test_c_to_f
+     expected_value = 50
+     actual_value = c_to_f(10)
+
+     assert_equal(expected_value, actual_value)
+   end
+
+   def test_c_to_f_negative
+     expected = -4
+     actual = c_to_f(-20)
+
+     assert_equal(expected, actual)
+   end
+
+   def test_c_to_f_freezing
+     expected = 32
+     actual = c_to_f(0)
+
+     assert_equal(expected, actual)
+   end
 end
