5a1ca8e9aa40371d3464989a5b21017ea0d1d61e
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-04-07 11:47:55 -0400

    Generate ContactsController with bin/rails g controller Contacts index new create show

--- 15-intro-to-rails/crm/config/routes.rb
+++ 15-intro-to-rails/crm/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'contacts/index'
+
+  get 'contacts/new'
+
+  get 'contacts/create'
+
+  get 'contacts/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
