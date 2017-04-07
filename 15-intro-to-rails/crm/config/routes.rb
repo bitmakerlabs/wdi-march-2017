f2c29f9c42ff61a8808b74fc6392d166f4496738
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-04-07 11:55:14 -0400

    Implement Contacts#index

--- 15-intro-to-rails/crm/config/routes.rb
+++ 15-intro-to-rails/crm/config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'contacts/index'
-
-  get 'contacts/new'
-
-  get 'contacts/create'
-
-  get 'contacts/show'
-
+  resources :contacts
+  root 'contacts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
