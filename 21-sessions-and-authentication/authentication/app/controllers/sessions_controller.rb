b7ea591de7c6c507b967713a60847fd2daba87db
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-04-19 12:11:01 -0400

    Intro to session hash

--- 21-sessions-and-authentication/authentication/app/controllers/sessions_controller.rb
+++ 21-sessions-and-authentication/authentication/app/controllers/sessions_controller.rb
@@ -8,6 +8,9 @@ class SessionsController < ApplicationController
     if u && u.authenticate(session_params[:password])
       flash[:notice] = 'Successfully logged in!'
 
+      # Remember the logged in user
+      session[:user_id] = u.id
+
       # In a real app, redirect to dashboard_url or something
       redirect_to root_url
     else
@@ -27,3 +30,8 @@ private
   end
 
 end
+
+# These 3 hashes are always available to you in a Rails controller
+# params
+# flash
+# session
