0f92be8da177022c5973ce7b05f07ec0b79a4af6
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-04-19 11:17:41 -0400

    Remove useless create view, add restful users routes

--- 21-sessions-and-authentication/authentication/config/routes.rb
+++ 21-sessions-and-authentication/authentication/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
+  resource :users, only: %i(new create)
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
