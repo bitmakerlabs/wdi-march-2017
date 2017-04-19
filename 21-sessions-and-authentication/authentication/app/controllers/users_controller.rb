6e846f59643f400d662b2d6a996765e49ec21fd4
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-04-19 11:29:53 -0400

    Implement UsersController#create

--- 21-sessions-and-authentication/authentication/app/controllers/users_controller.rb
+++ 21-sessions-and-authentication/authentication/app/controllers/users_controller.rb
@@ -4,5 +4,21 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      # In a real app, redirect to dashboard_url or something
+      redirect_to root_url
+    else
+      render :new
+    end
+
   end
+
+private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
 end
