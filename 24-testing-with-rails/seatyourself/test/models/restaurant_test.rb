d32430ae550230ade1e4b0be8bc02504ee3574f2
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-04-24 10:33:25 -0400

    First simple test

--- 24-testing-with-rails/seatyourself/test/models/restaurant_test.rb
+++ 24-testing-with-rails/seatyourself/test/models/restaurant_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'two restaurants exist' do
+    assert_equal 2, Restaurant.all.size
+  end
+
 end
