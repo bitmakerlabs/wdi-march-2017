78265fd56979671a4a495975b8c49b8ba33118dd
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-04-24 10:23:43 -0400

    Generate Restaurant model with bin/rails generate model Restaurant name:string address:string capacity:integer, and run migrations

---
+++ 24-testing-with-rails/seatyourself/db/migrate/20170424142047_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[5.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.integer :capacity
+
+      t.timestamps
+    end
+  end
+end
