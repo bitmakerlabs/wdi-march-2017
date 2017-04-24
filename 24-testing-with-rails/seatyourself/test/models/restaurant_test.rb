7eb2298b5ecff6b7f6a945baec0beac60040d264
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-04-24 11:00:29 -0400

    Use setup block

--- 24-testing-with-rails/seatyourself/test/models/restaurant_test.rb
+++ 24-testing-with-rails/seatyourself/test/models/restaurant_test.rb
@@ -2,14 +2,17 @@ require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
 
+  setup do
+    @big   = FactoryGirl.create :restaurant, capacity: 100
+    @small = FactoryGirl.create :restaurant, capacity: 25
+  end
+
   test 'two restaurants exist' do
-    2.times { FactoryGirl.create :restaurant }
     assert_equal 2, Restaurant.all.size
   end
 
   test 'small restaurant has small capacity' do
-    small = FactoryGirl.create :restaurant, capacity: 25
-    assert small.capacity <= 25
+    assert @small.capacity <= 25
   end
-
+  
 end
