c4166e85e845d58be032d0d43be1b0847f2e996d
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-05-01 11:26:41 -0400

    Implement load-monsters button

--- 30-intro-to-ajax/ajax.js
+++ 30-intro-to-ajax/ajax.js
@@ -1,3 +1,24 @@
 $(function() {
 
+  $('#load-monsters').on('click', function() {
+    console.log('Before .ajax');
+
+    $.ajax({
+      url: 'http://monsters-api.herokuapp.com/monsters',
+      method: 'GET',
+      dataType: 'html',
+      data: { limit: 2 },
+    }).done(function(data) {
+      console.log('.ajax done')
+      $('body').append(data);
+    }).fail(function(jqXHR, textStatus) {
+      console.log('.ajax FAILED with ' + jqXHR.status + ' ' + jqXHR.statusText);
+      console.log(jqXHR);
+    }).always(function() {
+      console.log('.ajax request finished');
+    });
+
+    console.log('After .ajax');
+  });
+
 });
