dc4b00f82b9bd4be2042b51209c825d2ec8c7d9d
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-05-02 10:43:01 -0400

    Load robots show into #robot-details box

--- 31-ajax-and-json/robots/app/assets/javascripts/robots.js
+++ 31-ajax-and-json/robots/app/assets/javascripts/robots.js
@@ -3,4 +3,21 @@
 
 $(function() {
 
+  // Step 1: Listen to the click event on the link
+  $('.robot > a').on('click', function(event) {
+
+    // Step 2: Prevent the default beahaviour of clicking on a link
+    event.preventDefault();
+
+    // Step 3: Make the ajax request
+    url = $(this).attr('href');
+    $.ajax({
+      url: url,
+      method: 'GET'
+    }).done(function(data) {
+      // Step 4: Update the page
+      $('#robot-details').html(data);
+    });
+
+  });
 });
